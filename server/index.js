1d5e0a69197cc24829f12b6e21110e4feeda8ad4
anija-cpu <m.noudo.masato@gmail.com>
2026-05-26 00:02:31 +0900

    remove node_modules from tracking

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: '*' }
+});
+
+io.on('connection', (socket) => {
+  console.log('接続:', socket.id);
+});
+
+server.listen(3001, () => {
+  console.log('サーバー起動: http://localhost:3001');
+});
